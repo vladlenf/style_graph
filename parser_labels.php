383c66034f019b1e08188a7684c9265e6f7016ab
vladlenf <115593365+vladlenf@users.noreply.github.com>
2024-06-05 20:21:28 +0300

    Update parser_labels.php

--- parser_labels.php
+++ parser_labels.php
@@ -44,4 +44,36 @@ fclose($mergedFile);
 
 // Запись обновленных данных обратно в файл labels.csv
 file_put_contents('labels.csv', implode("\n", $lines));
+
+//Удаляем повторы с маленьким количестом помет из файла merged_labels.csv
+// Чтение файла merged_labels.csv
+$lines = file('merged_labels.csv', FILE_IGNORE_NEW_LINES);
+
+// Создание нового файла filtered_labels.csv и запись данных
+$filteredFile = fopen('filtered_labels.csv', 'w');
+
+// Создание ассоциативного массива для хранения данных
+$filteredData = [];
+
+// Проход по каждой строке файла
+foreach ($lines as $line) {
+    list($firstColumn, $secondColumn) = explode('$', $line);
+
+    // Если значение второго столбца уже существует в массиве
+    if (array_key_exists($secondColumn, $filteredData)) {
+        // Сравниваем длину первого столбца и заменяем, если новая строка имеет более длинный первый столбец
+        if (strlen($firstColumn) > strlen($filteredData[$secondColumn])) {
+            $filteredData[$secondColumn] = $firstColumn;
+        }
+    } else {
+        $filteredData[$secondColumn] = $firstColumn;
+    }
+}
+
+// Записываем отфильтрованные данные в новый файл
+foreach ($filteredData as $secondColumn => $firstColumn) {
+    fwrite($filteredFile, $firstColumn . '$' . $secondColumn . "\n");
+}
+
+fclose($filteredFile);
 ?>
